2a7a6a82296286c26a50eeeb86a29ee0b5e3663c
LeeYi-user <khes60122@gmail.com>
2024-05-12 21:25:32 +0800

    add game cover

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const app = new Application();
 app.use(async (ctx) => {
   console.log('path=', ctx.request.url.pathname)
   await send(ctx, ctx.request.url.pathname, {
-    // root: `${Deno.cwd()}/public/`,
     root: Deno.cwd()+'/public/',
     index: "index.html",
   });
 });
 
-console.log('start at : http://127.0.0.1:8000')
+console.log('start at : http://127.0.0.1:8080')
 console.log('cwd=', Deno.cwd())
-await app.listen({ port: 8000 });
+await app.listen({ port: 8080 });
